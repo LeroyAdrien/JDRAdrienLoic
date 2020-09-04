eec63e497ef40ad3a3d9fec752a83c687b4cdf4c
LeroyAdrien <61692781+LeroyAdrien@users.noreply.github.com>
2020-09-04 16:45:27 +0200

    Add files via upload

---
+++ FileParsing.py
@@ -0,0 +1,18 @@
+import re 
+import numpy as np
+import fnmatch
+import os
+
+
+class FileParser(self,chemin):
+
+        def __init__(self):
+        
+                m_cheminObjectTypePattern'*[nN][bB][oO][fF][eE][nN]+[eE][mM][iI][eE][sS]'
+                m_cheminObjectChoicePattern
+                m_cheminNbOfEnnemiesPattern
+        
+                with os.scandir(self.chemin) as entries:
+                
+        
+        
